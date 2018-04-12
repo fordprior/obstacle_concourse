3333acfdc01623c81775574ad897a36e64f53088
Ford Prior <ford.prior@snagajob.com>
2018-04-12 15:56:57 -0400

    Lotsa fixes

---
+++ spec/app_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+require 'app'
+
+RSpec.describe "The ObstacleConcourse application" do
+  include Rack::Test::Methods
+
+  def app
+    ObstacleConcourse
+  end
+
+  describe 'a generic test for all my apps' do
+    it "doesn't return 500" do
+      get '/'
+      expect(last_response).to be_ok
+    end
+  end
+
+end
